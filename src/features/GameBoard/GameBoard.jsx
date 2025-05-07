fbc6e918566757fe2e76eabd773974132a5e569c
ernestoacosta75 <ernestoacostacuba@yahoo.com.mx>
2025-05-07 18:38:38 +0200

    Best practice: updating object state immutably

--- src/features/GameBoard/GameBoard.jsx
+++ src/features/GameBoard/GameBoard.jsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  // State hook to manage & update the game board
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectedSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevGameBoard) => {
+        const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];        
+        updatedBoard[rowIndex][colIndex] = 'X';
+        
+        return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                {/* Without using an arrow function in the onClick event, React
+                would execute the function immediatly during rendering rather that
+                when the button is clicked.
+                The arrow function creates a new function that will only be executed 
+                when the click event occurs.
+                This pattern is very common in React when we need to pass additional parameters
+                to an event handler. */}
+                <button onClick={() => handleSelectedSquare(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
